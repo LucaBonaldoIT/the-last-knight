499785141b6f09783b79977815e8f4a8809ec9ab
edinmilenko <154507977+edinmilenko@users.noreply.github.com>
2024-06-06 11:08:18 +0200

    finished level and testing

--- src/test/GameRoomTest.java
+++ src/test/GameRoomTest.java
@@ -1,19 +1,5 @@
 package test;
 
-import static org.junit.Assert.*;
-
-
-import main.java.com.mealforks.thelastknight.game.GameRoom;
-import main.java.com.mealforks.thelastknight.game.GameData;
-import main.java.com.mealforks.thelastknight.game.GamePlayer;
-import main.java.com.mealforks.thelastknight.game.GamePoint;
-import main.java.com.mealforks.thelastknight.game.GameTile;
-import main.java.com.mealforks.thelastknight.game.GameObject;
-import main.java.com.mealforks.thelastknight.game.GameArea;
-import main.java.com.mealforks.thelastknight.game.GameRoomType;
-import main.java.com.mealforks.thelastknight.game.GameRoomData;
-
-
 import main.java.com.mealforks.thelastknight.game.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,17 +8,20 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class GameRoomTest {
+import static org.junit.Assert.*;
 
-    private GameRoom room;
-    private GameData testData;
-    private GameObject object;
+public class GameRoomTest {
+    private GameRoom gameRoom;
+    private GameRoomType type;
+    private GameArea area;
+    private GamePoint point;
+    private GameTile tile;
+    private GameData data;
     private GamePlayer player;
 
 
     @Before
     public void setUp() {
-        // Creare array bidimensionali di GameTile e GameCollision
         GameTile[][] tiles = new GameTile[10][10];
         GameCollision[][] collisions = new GameCollision[10][10];
 
@@ -44,68 +33,21 @@ public class GameRoomTest {
             }
         }
 
+        type = GameRoomType.DEFAULT;
+        area = new GameArea(tiles, collisions);
+        point = new GamePoint(1, 1);
+        tile = GameTile.DOOR_EAST_WALL;
+        data = new GameData();
 
-
-        // Configura la stanza con una porta a est
-        HashMap<GamePoint, GameTile> doors = new HashMap<>();
-        doors.put(new GamePoint(1, 1), GameTile.DOOR_EAST_WALL);
-        room = new GameRoom("room1", GameRoomType.DEFAULT, new GameArea(tiles, collisions), new String[]{"room2", null, null, null}, doors, new GamePoint(0, 0), new HashMap<>());
-
-        // Configura i dati del gioco e il giocatore
-        testData = new GameData();
-        player = new GamePlayer(1, 1);
-        testData.setPlayer(player);
+        gameRoom = new GameRoom(type, area);
     }
 
     @Test
-    public void testUpdate() {
-        // Simula l'aggiornamento della stanza
-        GameData updatedData = room.update(testData);
-
-        // Verifica che la stanza successiva venga caricata
-        assertEquals("", updatedData.getGameRoom().getId());
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Before
-    public void setUp1() {
-
-        // Creare array bidimensionali di GameTile e GameCollision
-        GameTile[][] tiles = new GameTile[10][10];
-        GameCollision[][] collisions = new GameCollision[10][10];
-
-        // Inizializzare i tiles e collisions con valori di esempio
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                tiles[i][j] = GameTile.EMPTY;
-                collisions[i][j] = GameCollision.NONE;
-            }
-        }
-        // Configura la stanza con una porta
-        HashMap<GamePoint, GameTile> doors = new HashMap<>();
-        doors.put(new GamePoint(1, 1), GameTile.DOOR_EAST_WALL);
-        room = new GameRoom("room1", GameRoomType.DEFAULT, new GameArea(tiles, collisions), new String[]{"room2", null, null, null}, doors, new GamePoint(0, 0), new HashMap<>());
-
-        // Configura i dati del gioco
-        testData = new GameData();
-        object = new GameObject() {
+    public void testAddObjectToLoad() {
+        GameObject obj = new GameObject() {
             @Override
             public String getId() {
-                return "object1";
+                return "test";
             }
 
             @Override
@@ -120,7 +62,6 @@ public class GameRoomTest {
 
             @Override
             public void render(Graphics g) {
-                // Implementazione di esempio
             }
 
             @Override
@@ -133,134 +74,68 @@ public class GameRoomTest {
                 return false;
             }
         };
-        room.addObjectToLoad(object);
-    }
 
-    @Test
-    public void testSaveRoomData() {
-        // Salva i dati della stanza
-        room.saveRoomData(testData);
-
-        // Carica i dati della stanza in un nuovo oggetto GameData
-        GameData loadedData = room.loadRoomData(new GameData());
-
-        // Verifica che gli oggetti aggiunti siano presenti nei dati della stanza
-        assertTrue(loadedData.getGameObjects().contains(object));
-    }
-
-
-
-    @Before
-    public void setUp2() {
-        room = new GameRoom();
-        testData = new GameData();
-
-        // Crea un oggetto di test e aggiungilo a testData
-        object = new GameObject() {
-            @Override
-            public String getId() {
-                return "1";
-            }
-
-            @Override
-            public GamePoint getCoordinates() {
-                return new GamePoint(0, 0);
-            }
-
-            @Override
-            public int getIndex() {
-                return 0;
-            }
-
-            @Override
-            public void render(Graphics g) {
-                // Logica di rendering
-            }
-
-            @Override
-            public GameData update(GameData d) {
-                return d;
-            }
-
-            @Override
-            public boolean toDelete() {
-                return false;
-            }
-        };
-        testData.addObjectToScene(object);
-
-        // Salva i dati della stanza
-        room.saveRoomData(testData);
+        gameRoom.addObjectToLoad(obj);
+        assertEquals(1, gameRoom.getObjectsToLoad().size());
     }
 
     @Test
     public void testLoadRoomData() {
-        // Carica i dati della stanza
-        GameData loadedData = room.loadRoomData(new GameData());
-
-        // Verifica che i dati caricati contengano l'oggetto originale
-        ArrayList<GameObject> loadedObjects = loadedData.getGameObjects();
-        assertEquals(1, loadedObjects.size());
-        assertEquals("1", loadedObjects.get(0).getId());
+        gameRoom.saveRoomData(data);
+        GameData newData = gameRoom.loadRoomData(data);
+        assertNotNull(newData);
+        // Add further assertions based on the expected state of newData
     }
 
+    @Before
+    public void setUp1() {
+        // Inizializzare i tiles e collisions con valori di esempio
+        GameTile[][] tiles = new GameTile[10][10];
+        GameCollision[][] collisions = new GameCollision[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                tiles[i][j] = GameTile.EMPTY;
+                collisions[i][j] = GameCollision.NONE;
+            }
+        }
 
+        // Configura la stanza con una porta a est
+        HashMap<GamePoint, GameTile> doors = new HashMap<>();
+        doors.put(new GamePoint(1, 1), GameTile.DOOR_EAST_WALL);
+        gameRoom = new GameRoom("room1", GameRoomType.DEFAULT, new GameArea(tiles, collisions), new String[]{"room2", null, null, null}, doors, new GamePoint(0, 0), new HashMap<>());
 
-
-
-
-
-
-
-
-
-
-
-
-
+        // Configura i dati del gioco e il giocatore
+        data = new GameData();
+        player = new GamePlayer(1, 1);
+        data.setPlayer(player);
+    }
 
     @Test
-    public void testAddObjectToLoad() {
-        // Preparazione
-        GameRoom room = new GameRoom();
-        GameObject object = new GameObject() {
-            @Override
-            public String getId() {
-                return "1";
+    public void testUpdate() {
+        // Inizializzare i tiles e collisions con valori di esempio
+        GameTile[][] tiles = new GameTile[10][10];
+        GameCollision[][] collisions = new GameCollision[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                tiles[i][j] = GameTile.EMPTY;
+                collisions[i][j] = GameCollision.NONE;
             }
+        }
+        GameData testData = new GameData();
+        GameRoom room = new GameRoom("roomId", GameRoomType.DEFAULT, new GameArea(tiles, collisions), new String[]{"north", "east", "south", "west"}, new HashMap<>(), new GamePoint(1, 1), new HashMap<>());
+        GamePlayer player = new GamePlayer(1,1);
+        testData.setPlayer(player);
 
-            @Override
-            public GamePoint getCoordinates() {
-                return new GamePoint(0, 0);
-            }
+        // Simulate player moving through east door
+        GameData updatedData = room.update(testData);
 
-            @Override
-            public int getIndex() {
-                return 0;
-            }
+        assertEquals("", updatedData.getGameRoom().getId());
+    }
 
-            @Override
-            public void render(Graphics g) {
-                // Logica di rendering
-            }
-
-            @Override
-            public GameData update(GameData d) {
-                return d;
-            }
-
-            @Override
-            public boolean toDelete() {
-                return false;
-            }
-        };
-
-        // Esecuzione
-        room.addObjectToLoad(object);
-
-        // Confronto e verifica
-        ArrayList<GameObject> loadedObjects = room.getObjectsToLoad();
-        assertEquals(1, loadedObjects.size());
-        assertEquals("1", loadedObjects.get(0).getId());
+    @Test
+    public void testToDelete() {
+        assertFalse(gameRoom.toDelete());
     }
 }
+
+
