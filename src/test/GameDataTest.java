b59169f86d558686285f86956bfbf0743e923e56
Martinioini <123835828+Martinioini@users.noreply.github.com>
2024-06-15 08:51:23 +0200

    small fix

--- src/test/GameDataTest.java
+++ src/test/GameDataTest.java
@@ -10,9 +10,55 @@ public class GameDataTest {
 
     private static GameData gameData;
 
+    @Test
+    public void testConstructorInitializesFieldsCorrectly() {
+        assertNotNull(gameData.getGameSetting());
+        assertNotNull(gameData.getPlayerData());
+        assertNotNull(gameData.getPauseMenu());
+        assertEquals(GameState.READY, gameData.getGameState());
+        assertEquals(GameInput.NONE, gameData.getInput());
+        assertEquals(GameRoom.NONE, gameData.getGameRoom());
+        assertNotNull(gameData.getGameObjects());
+        assertNotNull(gameData.getInventory());
+        assertNotNull(gameData.getPlayer());
+    }
+
     @BeforeAll
     public static void setUp() {
         gameData = new GameData();
+    }
+
+    @Test
+    public void testGetCurrentLevel() {
+        GameLevel level = new GameLevel();
+        gameData.loadLevel(level);
+        assertEquals(level, gameData.getCurrentLevel());
+    }
+
+    @Test
+    public void testSetAndGetPlayerData() {
+        GamePlayerData playerData = new GamePlayerData();
+        gameData.setPlayerData(playerData);
+        assertEquals(playerData, gameData.getPlayerData());
+    }
+
+    @Test
+    public void testGetSave() {
+        GameDataSave save = gameData.getSave();
+        assertEquals(gameData.getCurrentLevel().getLevelIndex(), save.getCurrentLevel());
+        assertEquals(gameData.getPlayerData(), save.getPlayerData());
+    }
+    @Test
+    public void testSetAndGetGameState() {
+        gameData.setGameState(GameState.RUNNING);
+        assertEquals(GameState.RUNNING, gameData.getGameState());
+    }
+
+    @Test
+    public void testSetAndGetGameRoom() {
+        GameRoom room = new GameRoom();
+        gameData.setGameRoom(room);
+        assertEquals(room, gameData.getGameRoom());
     }
 
     @Test
